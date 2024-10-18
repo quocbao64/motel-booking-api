e2e69a74454005597e73c3f9729b7343b1bd8c46
quocbao64 <quocbao642002@gmail.com>
2024-10-18 10:41:07 +0700

    add geography sql data

--- public/province.sql
+++ public/province.sql
@@ -1,17 +1,63 @@
-create table province
-(
-    id            bigserial
-        primary key,
-    created_at    timestamp with time zone default CURRENT_TIMESTAMP,
-    updated_at    timestamp with time zone default CURRENT_TIMESTAMP,
-    deleted_at    timestamp with time zone,
-    province_name text,
-    province_type text
-);
-
-alter table province
-    owner to postgres;
-
-create index idx_province_deleted_at
-    on province (deleted_at);
-
+INSERT INTO public.province (id, province_name, province_type) VALUES (1, 'Hà Nội', 'Thành phố');
+INSERT INTO public.province (id, province_name, province_type) VALUES (2, 'Hà Giang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (4, 'Cao Bằng', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (6, 'Bắc Kạn', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (8, 'Tuyên Quang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (10, 'Lào Cai', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (11, 'Điện Biên', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (12, 'Lai Châu', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (14, 'Sơn La', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (15, 'Yên Bái', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (17, 'Hoà Bình', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (19, 'Thái Nguyên', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (20, 'Lạng Sơn', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (22, 'Quảng Ninh', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (24, 'Bắc Giang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (25, 'Phú Thọ', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (26, 'Vĩnh Phúc', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (27, 'Bắc Ninh', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (30, 'Hải Dương', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (31, 'Hải Phòng', 'Thành phố');
+INSERT INTO public.province (id, province_name, province_type) VALUES (33, 'Hưng Yên', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (34, 'Thái Bình', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (35, 'Hà Nam', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (36, 'Nam Định', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (37, 'Ninh Bình', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (38, 'Thanh Hóa', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (40, 'Nghệ An', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (42, 'Hà Tĩnh', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (44, 'Quảng Bình', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (45, 'Quảng Trị', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (46, 'Thừa Thiên Huế', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (48, 'Đà Nẵng', 'Thành phố');
+INSERT INTO public.province (id, province_name, province_type) VALUES (49, 'Quảng Nam', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (51, 'Quảng Ngãi', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (52, 'Bình Định', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (54, 'Phú Yên', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (56, 'Khánh Hòa', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (58, 'Ninh Thuận', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (60, 'Bình Thuận', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (62, 'Kon Tum', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (64, 'Gia Lai', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (66, 'Đắk Lắk', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (67, 'Đắk Nông', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (68, 'Lâm Đồng', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (70, 'Bình Phước', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (72, 'Tây Ninh', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (74, 'Bình Dương', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (75, 'Đồng Nai', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (77, 'Bà Rịa - Vũng Tàu', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (79, 'Hồ Chí Minh', 'Thành phố');
+INSERT INTO public.province (id, province_name, province_type) VALUES (80, 'Long An', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (82, 'Tiền Giang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (83, 'Bến Tre', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (84, 'Trà Vinh', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (86, 'Vĩnh Long', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (87, 'Đồng Tháp', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (89, 'An Giang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (91, 'Kiên Giang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (92, 'Cần Thơ', 'Thành phố');
+INSERT INTO public.province (id, province_name, province_type) VALUES (93, 'Hậu Giang', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (94, 'Sóc Trăng', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (95, 'Bạc Liêu', 'Tỉnh');
+INSERT INTO public.province (id, province_name, province_type) VALUES (96, 'Cà Mau', 'Tỉnh');
